6372bd02299ced3b2af3b5987254ba600f26c572
Shankrith <shankrith1618@gmail.com>
2021-03-18 15:41:31 +0530

    homing routine

--- homing/homing.ino
+++ homing/homing.ino
@@ -9,10 +9,11 @@
 
 
 
-int YOFFSET = -10;
+int YOFFSET = -50;
 int ZOFFSET = 130;
+int XOFFSET = -30;
 
-
+AccelStepper XMOTOR(1, 2, 5);
 AccelStepper YMOTOR(1, 3, 6);
 AccelStepper ZMOTOR(1, 4, 7);
 
@@ -54,14 +55,42 @@ void yhome() {
  YMOTOR.runToNewPosition(YOFFSET);
 
 }
+
+void xhome() {
+ int initial_xhome = 1;
+ //  Set Max Speed and Acceleration of each Steppers at startup for homing
+ XMOTOR.setMaxSpeed(100.0);      // Set Max Speed of Stepper (Slower to get better accuracy)
+ XMOTOR.setAcceleration(100.0);  // Set Acceleration of Stepper
+ YMOTOR.setMaxSpeed(100.0);      // Set Max Speed of Stepper (Slower to get better accuracy)
+ YMOTOR.setAcceleration(100.0);
+ // Start Homing procedure of Stepper Motor at startup
+ while (!digitalRead(XLIMIT)) {  // Make the Stepper move CCW until the switch is activated
+   XMOTOR.moveTo(initial_xhome);  // Set the position to move to
+   YMOTOR.moveTo(-initial_xhome);
+   initial_xhome++;  // Decrease by 1 for next move if needed
+   XMOTOR.run(); // Start moving the stepper
+   YMOTOR.run();
+   delay(5);
+ }
+ XMOTOR.setCurrentPosition(0);  // Set the current position as zero for now
+ XMOTOR.setMaxSpeed(50.0);      // Set Max Speed of Stepper (Slower to get better accuracy)
+ XMOTOR.setAcceleration(50.0);  // Set Acceleration of Stepper
+ XMOTOR.runToNewPosition(XOFFSET);
+
+}
 void setup(){
   pinMode(shield_en, OUTPUT);
   digitalWrite(shield_en,LOW);
   delay(5);
   pinMode(ZLIMIT, INPUT_PULLUP);
   pinMode(YLIMIT, INPUT_PULLUP);
+  pinMode(XLIMIT, INPUT_PULLUP);
   zhome();
+//  xhome();
   yhome();
+
+  xhome();
+  
  
 }
 
